f8f37ac80b1d6f960a09d51b924b2ea845399d3c
Thomas Andresen <thomas.andresen@earthgame.dk>
2013-07-02 13:47:46 +0200

    Testing

---
+++ lang/lang.en.js
@@ -0,0 +1,55 @@
+//English
+plugCubed.lang = {
+    disconnected          : "Disconnected at %1!",
+    reloading             : "Reloading the page in a few seconds.",
+    afk                   : "I'm away from keyboard.",
+    userCommands          : 'User Commands',
+    modCommands           : 'Moderation Commands',
+    commandsTitle         : 'plug&#179; Commands',
+    newVersion            : 'A new version of plug&#179; has been released. Your script will reload in a few seconds.',
+    ranks: {
+        you         : 'You',
+        regular     : 'Regular',
+        featureddj  : 'Featured DJ',
+        bouncer     : 'Bouncer',
+        manager     : 'Manager',
+        cohost      : 'Co-Host',
+        host        : 'Host',
+        ambassador  : 'Ambassador',
+        admin       : 'Admin'
+    },
+    join                  : 'User Join',
+    leave                 : 'User Leave',
+    curate                : 'User Curate',
+    stats                 : 'Song Stats',
+    updates               : 'Song Updates',
+    automuted             : '%1 auto-muted.',
+    autowoot              : 'Autowoot',
+    autojoin              : 'Autojoin',
+    userlist              : 'Userlist',
+    customchatcolors      : 'Custom Chat Colors',
+    afkstatus             : 'AFK Status',
+    notify                : 'Notify',
+    limitchatlog          : 'Limit Chat Log',
+    stream                : 'Stream',
+    emoji                 : 'Emoji',
+    unknownmoderation     : 'Unknown moderation',
+    usernotfound          : 'User not found',
+    currentlydjing        : 'Currently DJing',
+    notinwaitlistnorbooth : 'Not in waitlist nor booth',
+    inbooth               : '%1/%2 in booth',
+    inwaitlist            : '%1/%2 in waitlist',
+    status: {
+        idle      : 'Idle',
+        available : 'Available',
+        afk       : 'AFK',
+        working   : 'Working',
+        sleeping  : 'Sleeping'
+    },
+    vote: {
+        meh       : 'Meh',
+        woot      : 'Woot',
+        undecided : 'Undecided',
+        djing     : 'DJing'
+    }
+};
